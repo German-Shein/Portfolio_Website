a178ea94677e1f5b730c5a4ff8c80360f360a627
German Shein <germanshein1995@gmail.com>
2022-12-25 19:20:28 +0400

    Footer: Creating and utilizing the component

--- src/app/Views/About_Me/About_Me.component.ts
+++ src/app/Views/About_Me/About_Me.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 
 export class About_Me_Component
 {
-
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,11 @@ import { Instagram_Icon_Component } from './Icons/Instagram_Icon/Instagram_Icon.
 import { LinkedIn_Icon_Component } from './Icons/LinkedIn_Icon/LinkedIn_Icon.component';
 import { Top_Corner_Ornament_Component } from './Ornaments/Top_Corner/Top_Corner.component';
 import { Crown_Ornament_Component } from './Ornaments/Crown/Crown.component';
+import { Bottom_Corner_Ornament_Component } from './Ornaments/Bottom_Corner/Bottom_Corner.component';
+import { Footer_Component } from './Footer/Footer.component';
 
 @NgModule ({
-	declarations: [AppComponent, Header_Component, About_Me_Component, Freelancer_Icon_Component, GitHub_Icon_Component, Instagram_Icon_Component, LinkedIn_Icon_Component, Top_Corner_Ornament_Component, Crown_Ornament_Component],
+	declarations: [AppComponent, Header_Component, About_Me_Component, Freelancer_Icon_Component, GitHub_Icon_Component, Instagram_Icon_Component, LinkedIn_Icon_Component, Top_Corner_Ornament_Component, Crown_Ornament_Component, Bottom_Corner_Ornament_Component, Footer_Component],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent]
