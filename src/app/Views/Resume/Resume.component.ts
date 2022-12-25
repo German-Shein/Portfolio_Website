2ba5feb95ad3f909c167a237793fa94cbddbaaae
German Shein <germanshein1995@gmail.com>
2022-12-25 20:34:44 +0400

    Resume: Creating and utilizing the component

---
+++ src/app/Views/Resume/Resume.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component ({
+	selector: 'resume-component',
+	templateUrl: './Resume.component.html',
+	styleUrls: ['./Resume.component.scss']
+})
+
+export class Resume_Component
+{
+    
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent 
 {
-    View: string = "About Me"
+	View: string = "About Me"
 
 	Select_Active_View (View: string): void
 	{
