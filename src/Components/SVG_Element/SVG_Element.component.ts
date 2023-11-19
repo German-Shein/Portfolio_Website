603b48827ba7632efa50b8672c163caee57b9c48
German Shein <germanshein1995@gmail.com>
2023-11-19 17:51:06 +0400

    SVG: Finalizing the component

--- src/Components/SVG_Element/SVG_Element.component.ts
+++ src/Components/SVG_Element/SVG_Element.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { SafeHtml } from "@angular/platform-browser";
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component ({
 	selector: "svg-element",
-	template: "<svg [attr.class]='Classes' [attr.fill]='Color' [attr.height]='Height' innerHtml='{{Inner_SVG}}' [attr.width]='Width' [attr.viewBox]='View_Box' xmlns='http://www.w3.org/2000/svg'></svg>",
+	template: "<svg [attr.class]='Classes' [attr.fill]='Color' [attr.height]='Height' [innerHTML]='Inner_SVG' [attr.width]='Width' [attr.viewBox]='View_Box' xmlns='http://www.w3.org/2000/svg'></svg>",
 	styles: ["svg {width: inherit; height: inherit;}"]
 })
 
@@ -12,18 +12,19 @@ export class SVG_Element_Component implements OnInit
     @Input () Classes: string = '';
 	@Input () Color: string = '';
 	@Input () Height: string = '';
-	@Input () Inner_SVG: string = '';
+	@Input () Inner_SVG: SafeHtml | string | null = '';
 	@Input () SVG_Path: string = '';
 	@Input () View_Box: string = '';
 	@Input () Width: string = '';
 
-	constructor () 
+	constructor (private Sanitizer: DomSanitizer) 
 	{
 		
 	}
 
 	ngOnInit (): void 
 	{
+		this.Inner_SVG = this.Sanitizer.bypassSecurityTrustHtml (this.Inner_SVG as string);
 		console.log (this.Inner_SVG);
 	}
 }
