555d79e5e615493c0ca665052852658f783922dd
German Shein <germanshein1995@gmail.com>
2023-12-20 00:04:55 +0400

    Adding al waseet icon

--- src/app/Views/Portfolio/Portfolio.component.ts
+++ src/app/Views/Portfolio/Portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import SVG_Images from 'src/assets/SVG_Images.json';
 
 @Component ({
 	selector: 'portfolio-component',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
 
 export class Portfolio_Component
 {
-    
+    al_waseet_Logo: string = SVG_Images.al_waseet_Logo;
 }
