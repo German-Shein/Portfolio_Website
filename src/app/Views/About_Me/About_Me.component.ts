6600f6c6751e92149b0a8da311e3e8a37f6beec8
German Shein <germanshein1995@gmail.com>
2023-12-26 02:02:51 +0400

    Adding Behance, Dribbble, Threads and X Social Media links

--- src/app/Views/About_Me/About_Me.component.ts
+++ src/app/Views/About_Me/About_Me.component.ts
@@ -9,8 +9,12 @@ import SVG_Icons from '../../../assets/SVG_Icons.json';
 
 export class About_Me_Component
 {
+	Behance_Icon: string = SVG_Icons.Behance_Icon;
+	Dribbble_Icon: string = SVG_Icons.Dribbble_Icon;
     Freelancer_Icon: string = SVG_Icons.Freelancer_Icon;
 	GitHub_Icon: string = SVG_Icons.GitHub_Icon;
 	Instagram_Icon: string = SVG_Icons.Instagram_Icon;
 	LinkedIn_Icon: string = SVG_Icons.LinkedIn_Icon;
+	Threads_Icon: string = SVG_Icons.Threads_Icon;
+	X_Icon: string = SVG_Icons.X_Icon;
 }
