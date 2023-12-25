26fc7e4a86d89ea66f0ccb63b7f6a9aac50f09c3
German Shein <germanshein1995@gmail.com>
2023-12-25 22:57:40 +0400

    Graphic Design Masterclass: Finishing the exhibit

--- src/app/Views/Portfolio/Portfolio.component.ts
+++ src/app/Views/Portfolio/Portfolio.component.ts
@@ -11,4 +11,7 @@ export class Portfolio_Component
 {
     al_waseet_Logo: string = SVG_Images.al_waseet_Logo;
 	Sandwitches_Logo: string = SVG_Images.Sandwitches_Logo;
+	Space_Badge: string = SVG_Images.Space_Badge;
+	Video_Flux_Icon: string = SVG_Images.Video_Flux_Icon;
+	Video_Flux_Logo: string = SVG_Images.Video_Flux_Logo;
 }
