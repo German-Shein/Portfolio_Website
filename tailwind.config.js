37af0f349ff3a479c2342beb4742c1881344ad29
German Shein <germanshein1995@gmail.com>
2024-01-04 14:32:48 +0400

    Responsiveness: Defining custom breakpoints and adjusting the UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports =
 	theme: 
 	{
 		extend: {},
+		screens: 
+		{
+			'sm': '640px',
+			'md': '1024px',
+			'lg': '1440px',
+			'xl': '1920px',
+			'2xl': '2560px',
+			'3xl': '3840px'
+		},
 	},
 	plugins: [],
   }
